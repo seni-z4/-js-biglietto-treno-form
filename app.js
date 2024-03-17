e7cf5500bf9686cc40f8a0dae7bd9f65721a1559
seniya <seniyathri@gmail.com>
2024-03-17 16:54:16 +0100

    feat: the index page has been edited and in the js file comments has been added

--- app.js
+++ app.js
@@ -1,47 +1,12 @@
-console.log("hello- world");
+console.log("hello-world");
 
-// come abbiamo fatto i classe per il calcole delle calorie, trasformate l’esercizio del calcolo del biglietto del treno in modo da usare un form di bootstrap.
-// create una pagina html che contenga due input di tipo text: una per inserire i km e una per inserire l’eta
-// aggiungete un button
-// L’utente potrà compilare il form e cliccare il pulsante per calcolare il prezzo. La logica è la stessa del vecchio esercizio, ma ogni volta che l’utente clicca sul pulsante dovete:
-// recuperare i km dall’input dei km
-// recuperare l’eta dall’input dell’età
-// calcolare il prezzo
-// stampare il prezzo nella pagina html in un semplice div
+//creare un form con boostrap
+//create un input di km
+//create un input di age
+//creare un button
 
-//create a input for the km
-//create a input for the age
-//create a button
-
-const kmInputElement = document.getElementById("km");
-const ageInputElement = document.getElementById("age");
-const buttonSubmit = document.getElementById("submit");
-const ticketElement = document.getElementById("ticket");
-
-console.dir(kmInputElement, ageInputElement);
-
-const km = kmInputElement.value;
-const age = ageInputElement.value;
-
-console.log(km, age);
-
-buttonSubmit.addEventListener("click", function () {
-  const km = parseInt(kmInputElement.value);
-  const age = parseInt(ageInputElement.value);
-
-  // sconto = 0;
-  if (age < 18) {
-    sconto = age * 0.2;
-  } else if (age > 65) {
-    sconto = age * 0.4;
-  } else {
-    sconto = age * 1;
-  }
-
-  console.log(km, typeof km, age);
-  console.log(sconto.toFixed(2));
-
-  ticketElement.innerHTML = `Totally tavled (${km} km) and  has  been suscessfull purched the ticket here is yout total prize ${
-    sconto.toFixed(2) + " &euro;"
-  }`;
-});
+//quando l'utente click la cta
+//-recupeare i dati del km
+//-recuperare i dati del age
+//-calcolare il prezzo finale
+//- deve stampare il prezzo totale nella pagina html
